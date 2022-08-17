e1323045f1f38100a795fe0ca0461e54b4f5c384
李亚南 <735835816@qq.com>
2022-08-17 21:53:16 +0800

    update

---
+++ ddd-service-parent/ddd-service-domain/src/main/java/com/lynjava/ddd/domain/external/iam/dto/IamExtInDto.java
@@ -0,0 +1,10 @@
+package com.lynjava.ddd.domain.external.iam.dto;
+
+import lombok.Data;
+
+/**
+ * 防腐层输入输出dto命名方式为：XXXExtInDto、XXXExtOutDto
+ */
+@Data
+public class IamExtInDto {
+}
---
+++ ddd-service-parent/ddd-service-domain/src/main/java/com/lynjava/ddd/domain/external/iam/dto/IamExtOutDto.java
@@ -0,0 +1,7 @@
+package com.lynjava.ddd.domain.external.iam.dto;
+
+import lombok.Data;
+
+@Data
+public class IamExtOutDto {
+}
