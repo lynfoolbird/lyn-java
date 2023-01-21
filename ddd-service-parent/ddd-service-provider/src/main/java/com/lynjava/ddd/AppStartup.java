0c81201861569ae39c0b50fbb61cf89d26e57618
李亚南 <735835816@qq.com>
2023-01-21 14:00:31 +0800

    应用启动类注释

--- ddd-service-parent/ddd-service-provider/src/main/java/com/lynjava/ddd/AppStartup.java
+++ ddd-service-parent/ddd-service-provider/src/main/java/com/lynjava/ddd/AppStartup.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * 应用启动类
+ *
+ * @author li
+ */
 @SpringBootApplication(scanBasePackages = {"com.lynjava.ddd"})
 //@ComponentScan(basePackages ={ "com.lynjava.ddd"})
 @ImportResource(locations = { "classpath:config/*.xml" })
-public class DddServiceApplication extends SpringBootServletInitializer {
+public class AppStartup extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(DddServiceApplication.class);
+        SpringApplication.run(AppStartup.class);
     }
 }
